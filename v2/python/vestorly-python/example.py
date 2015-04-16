52aa2c06299ec413a9223e8d5352be8387818d7b
Joseph Misiti <joseph.misiti@gmail.com>
2015-04-16 16:14:43 -0400

    removing old clients

---
+++ v2/python/vestorly-python/example.py
@@ -0,0 +1,17 @@
+
+
+import vestorly
+api_key = 'eyJwYXlsb2FkIjoiNTUxYWQyZWRjOThmMTM0MzY0MDAwMDQ0IiwiY3JlYXRlZF9vbiI6MTQyODk0OTQxMywic2lnbmF0dXJlIjoiVzN2L3crVWZ0ZDBmc3B5T2pSY1FPd3VqcFJmNnZsS0tVRURrZlFJZGZpdz0ifQ'
+
+
+client = vestorly.ApiClient(host='https://staging.vestorly.com/',headerName='x-vestorly-auth', headerValue=api_key)
+
+
+# api =  vestorly.ArticlesApi(client)
+# print api.index()
+
+#api = vestorly.AdvisorsApi(client)
+#print api.show()._id
+
+api = vestorly.SourcesApi(client)
+print api.index()
