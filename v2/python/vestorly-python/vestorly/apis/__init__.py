3f7e531c83f154362eac44c04d368262fafaa92c
jondayton <jondayton@gmail.com>
2017-06-16 13:17:44 -0400

    update params for member apis

--- v2/python/vestorly-python/vestorly/apis/__init__.py
+++ v2/python/vestorly-python/vestorly/apis/__init__.py
@@ -1,16 +1,15 @@
 from __future__ import absolute_import
 
 # import apis into api package
-from .groups_api import GroupsApi
-from .sessions_api import SessionsApi
-from .posts_api import PostsApi
-from .newslettersettings_api import NewslettersettingsApi
-from .memberreports_api import MemberreportsApi
 from .advisors_api import AdvisorsApi
-from .events_api import EventsApi
-from .sources_api import SourcesApi
-from .members_api import MembersApi
-from .newsletters_api import NewslettersApi
 from .articles_api import ArticlesApi
-from .memberevents_api import MembereventsApi
-
+from .events_api import EventsApi
+from .groups_api import GroupsApi
+from .member_events_api import MemberEventsApi
+from .member_reports_api import MemberReportsApi
+from .members_api import MembersApi
+from .newsletter_settings_api import NewsletterSettingsApi
+from .newsletters_api import NewslettersApi
+from .posts_api import PostsApi
+from .sessions_api import SessionsApi
+from .sources_api import SourcesApi
