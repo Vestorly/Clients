7b3f9bcee126854e0404011feb4a70ec800e841d
Jon Dayton <jondayton@gmail.com>
2017-06-20 14:35:29 -0400

    update params for member apis + un-nest directories (#16)
    * update params for member apis
    
    * un-nest directories

---
+++ v2/python/vestorly/apis/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .advisors_api import AdvisorsApi
+from .articles_api import ArticlesApi
+from .events_api import EventsApi
+from .groups_api import GroupsApi
+from .member_events_api import MemberEventsApi
+from .member_reports_api import MemberReportsApi
+from .members_api import MembersApi
+from .newsletter_settings_api import NewsletterSettingsApi
+from .newsletters_api import NewslettersApi
+from .posts_api import PostsApi
+from .sessions_api import SessionsApi
+from .sources_api import SourcesApi
